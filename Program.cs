0c7b836ee68c8b608b6c254e26c0d713efe9b202
IonPAX <d.an.s@icloud.com>
2022-02-16 15:11:32 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO.Ports;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            Console.WriteLine("Available Ports:");
+            foreach (string s in SerialPort.GetPortNames()) // Смотрим, какие СОМ порты доступны
+            {
+                Console.WriteLine("   {0}", s); 
+            }
+            */
+            string portName = "COM" + portNumSetup(); // Запрашиваем и устанавливаем номер порта СОМ
+
+            Console.WriteLine("Available IO speeds for {0} : 1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200", portName);
+
+            int portIOSpeed = portSpeedSetup(); // Запрашиваем и устанавливаем скорость СОМ порта
+
+            Task.Factory.StartNew(() => StartListen(portName, portIOSpeed));
+            Console.ReadLine();
+        }
+        public static void StartListen(string portName, int portIOSpeed)
+        {
+            using (SerialPort port = new SerialPort(portName, portIOSpeed))
+            {
+                Console.Write("Start connecting to {0} with speed {1} ...", portName, portIOSpeed);
+                try
+                {
+                    port.Open(); // пытаемся открыть СОМ порт с установленными параметрами
+                }
+                catch (Exception e) // ловим ошибки при открытии СОМ порта
+                {
+                    Console.WriteLine("\nSomething went wrong:\n {0}", e); // не смогли открыть СОМ порт 
+                    return ;
+                }
+                Console.WriteLine("OK");
+                Console.WriteLine("Listening ...");
+
+
+                while (true)
+                {
+                    if (port.BytesToRead != 0)
+                    {
+                        string result = port.ReadExisting(); // читаем, что исходит их СОМ порта
+                        
+                            Console.WriteLine("Received: {0}", result); // выводим то, что услышали
+                       
+                    }
+                    Thread.Sleep(500);
+                }
+            }
+        }
+
+        public static string portNumSetup() // устанавливаем номер СОМ порта
+        {
+            Console.WriteLine("Available Ports:");
+            foreach (string s in SerialPort.GetPortNames()) // Смотрим, какие СОМ порты доступны
+            {
+                Console.WriteLine("   {0}", s);
+            }
+            Console.Write("Please enter COM port number :");
+            string portNum = Console.ReadLine();
+            return portNum;
+        }
+        public static int portSpeedSetup() // устанавливаем скорость СОМ порта
+        {
+            Console.Write("Please enter IO speed for COM port number :");
+            int portSpeed = Convert.ToInt32(Console.ReadLine());
+            return portSpeed;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Serial_IO_WinTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Serial_IO_WinTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("2fe2ad7f-d861-4d02-bbe2-63e8a7bfb532")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
